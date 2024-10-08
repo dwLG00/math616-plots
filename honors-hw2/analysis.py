1754d1757bf57895b3f4e528389bdbd7e4f29613
dwLG00 <wallace.dylan00@gmail.com>
2024-10-08 13:27:54 -0500

    Fixed RC sum reconstruction issue, but each RC still has very small amp

--- honors-hw2/analysis.py
+++ honors-hw2/analysis.py
@@ -34,11 +34,13 @@ def load(dir):
 
     return rmm_np
 
-def generate_figure_1(rmm_np):
+def generate_figure_1(rmm_np, mode_no=None):
     '''Generate a figure similar to figure 1'''
 
     start = 365
     end=730
+
+    modecode = "z%s " % mode_no if mode_no else ""
 
     data = rmm_np[start:end, :]
     rmm1 = data[:, 0]
@@ -47,13 +49,13 @@ def generate_figure_1(rmm_np):
     plt.plot(rmm1)
     plt.xlabel("Days since Jan 1, 2000")
     plt.ylabel("Centered RMM1")
-    plt.title("RMM1 between Jan 1, 2000 - Dec 31, 2000")
+    plt.title(modecode + "RMM1 between Jan 1, 2000 - Dec 31, 2000")
     plt.show()
 
     plt.plot(rmm2)
     plt.xlabel("Days since Jan 1, 2000")
     plt.ylabel("Centered RMM2")
-    plt.title("RMM2 between Jan 1, 2000 - Dec 31, 2000")
+    plt.title(modecode + "RMM2 between Jan 1, 2000 - Dec 31, 2000")
     plt.show()
 
     # Generate histogram pdfs
@@ -71,22 +73,40 @@ def generate_figure_1(rmm_np):
     plt.plot(bins1, density1(bins1))
     plt.xlabel("RMM1 value")
     plt.ylabel("Probability density")
-    plt.title("RMM1 PDF")
+    plt.title(modecode + "RMM1 PDF")
     plt.show()
 
     _, bins2 = np.histogram(rmm2, bins=50, range=(rmm2_min, rmm2_max))
     plt.plot(bins2, density1(bins2))
     plt.xlabel("RMM2 value")
     plt.ylabel("Probability density")
-    plt.title("RMM2 PDF")
+    plt.title(modecode + "RMM2 PDF")
     plt.show()
 
-def trad_ssa(rmm_np, M=51):
+def generate_figure_12(rmm_np, mode_no=None):
+    start = 365
+    end = 365 + 60
+
+    modecode = "z%s " % mode_no if mode_no else ""
+
+    # Transpose rmm1, rmm2 data into polar
+    rmm1 = rmm_np[start:end, 0]
+    rmm2 = rmm_np[start:end, 1]
+    r = np.sqrt(rmm1**2 + rmm2**2)
+    phi = np.arctan2(rmm2, rmm1)
+
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    ax.plot(phi, r)
+    ax.set_rmax(max(r)*1.5)
+    ax.grid(True)
+    ax.set_title(modecode + "Phase diagrams of RMM1 and RMM2 raw data")
+    plt.show()
+
+def trad_ssa(rmm_np, M=51, gen_z=4):
     # Computes traditional SSA algorithm on rmm with window size M
 
     N = rmm_np.shape[0]
 
-    print("Means: %s, %s" % (mean1, mean2))
     print(rmm_np, rmm_np.shape)
 
     # Compute window size, and construct time-lagged datapoint array
@@ -99,10 +119,15 @@ def trad_ssa(rmm_np, M=51):
     C = X.dot(XT) / window_size
     eigenvalues, eigenvectors = LA.eig(C)
 
+    # Sort eigenvectors in order of eigenvalue magnitude
+    eigenorder = eigenvalues.argsort()
+    #eigenvectors = np.sqrt(eigenvalues) * eigenvectors
+    eigenvectors = eigenvectors[eigenorder[::-1]]
+
     def MLU(t):
         # Sum count and lower/upper bounds for sum (used for generating z(t))
         if t < M - 1:
-            return (t + 1, 0, t+1)
+            return (t + 1, 0, t + 1)
         elif M - 1 <= t < N - M + 1:
             return (M, 0, M)
         elif N - M + 1 <= t < N:
@@ -119,10 +144,43 @@ def trad_ssa(rmm_np, M=51):
 
     # Construct RCs
     zs = []
-    for i in tqdm(range(eigenvalues.size)):
+    for i in tqdm(range(min(gen_z, eigenvalues.size))):
         v = eigenvectors[i]
+        v = v / LA.norm(v) # Normalize each eigenvector
         phi = XT.dot(v) # Projection of v onto the timelagged data space
         w = np.reshape(v, (M, 2)) # Group v into a sequence of pair vectors (RMM1, RMM2)
         zs.append(generate_t(phi, w))
 
-    print(zs)
+    return zs
+
+def cumul_plot(zs, n=10):
+    start = 365
+    end = 730
+
+    cumul = 0
+    for i in range(n):
+        cumul = zs[i] + cumul
+        rmm1 = cumul[start:end, 0]
+        plt.plot(rmm1)
+
+    plt.xlabel("Days since Jan 1, 2000")
+    plt.ylabel("Centered RMM1")
+    plt.title("Cumulative RC RMM1 between Jan 1, 2000 - Dec 31, 2000")
+    plt.show()
+
+
+if __name__ == '__main__':
+    rmm_np = load("data")
+    zs = trad_ssa(rmm_np, gen_z=1000)
+
+    # Generate plots using raw rmm data
+    generate_figure_1(rmm_np)
+    generate_figure_12(rmm_np)
+
+    '''
+    for i, z in enumerate(zs):
+        generate_figure_1(z, mode_no=i+1)
+        #generate_figure_12(z, mode_no=i+1)
+    '''
+    cumul_plot(zs, n=len(zs))
+    #cumul_plot(zs, n=5)
